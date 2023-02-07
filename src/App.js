a07121d1f1d21a6c78bfa3301abddcd41d8899f4
boclaypool <bo@ipx.org>
2023-02-06 18:42:13 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import BeerCard from './BeerCard';
+// import ListBeers from './ListBeers';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      arrayOfBeer: [],
+      isClicked: true,
+      isLiked: [],
+      // searchTerm: '',
+    };
+  }
+
+  componentDidMount() {
+    axios.get('https://api.punkapi.com/v2/beers').then((res) => {
+      const arrayOfBeer = res.data;
+      this.setState({ arrayOfBeer });
+    });
+  }
+
+  // handleChange = (e) => {
+  //   this.setState({
+  //     [e.target.name]: e.target.value,
+  //   });
+  // };
+
+  // filterSearch(term) {
+  //   return (item) => {
+  //     return item.name.toLowerCase().includes(term.toLowerCase());
+  //   };
+  // }
+
+  render() {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          {/* <form>
+            <input
+              name='searchTerm'
+              type='text'
+              value={this.state.searchTerm}
+              onChange={(e) => {
+                this.handleChange();
+              }}
+              placeholder='Search for your favorite beer...'
+            ></input>
+          </form>
+          {this.state.searchTerm ? (
+            <ListBeers
+              beer={this.state.arrayOfBeer.filter(
+                this.filterSearch(this.state.searchTerm)
+              )}
+            />
+          ) : (
+            <ListBeers beer={this.state.arrayOfBeer} />
+          )} */}
+
+          <ol>
+            {this.state.arrayOfBeer.map((beer, index) => {
+              return (
+                <BeerCard
+                  key={index}
+                  name={beer.name}
+                  image_url={beer.image_url}
+                  first_brewed={beer.first_brewed}
+                  tagline={beer.tagline}
+                  abv={beer.abv}
+                  description={beer.description}
+                  handleLike={this.handleLike}
+                />
+              );
+            })}
+          </ol>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
